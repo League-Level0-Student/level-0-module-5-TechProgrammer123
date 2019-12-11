0013b3686606e22a39d970deb8e4cac5b6bc56a6
TechProgrammer123 <43911240+TechProgrammer123@users.noreply.github.com>
2019-12-11 14:56:46 -0800

    :smile:

--- src/_07_skill_practice/SkillPractice.java
+++ src/_07_skill_practice/SkillPractice.java
@@ -1,5 +1,14 @@
 package _07_skill_practice;
 
 public class SkillPractice {
+	public static void main(String[] args) {
 
-}
+		SkillPractice skills = new SkillPractice();
+		           skills.skill1(  );
+		           skills.skill2();
+		           skills.skill3();
+		           skills.skill4();
+		           skills.skill5();
+		}
+	void skill1() {
+	}}
